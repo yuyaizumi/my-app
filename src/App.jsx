647d3aebc1ee833e53ce0bc48218868ca62a145a
clc95285 <yuya.izumi18@gmail.com>
2022-12-08 14:26:48 +0900

    4-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 export const App = () => {
+    // ボタンを推したときに実行する関数を定義
+    const onClickButton = () => {
+        alert();
+    };
+
+    // cssオブジェクト
+    const contentStyle = {
+        color: "blue",
+        fontSize: "20px"
+    };
 
     return (
         <>
-        <h1> こんにちは! </h1>
-        <p> お元気ですか？ </p>
+        <h1 style={{ color: "red"}}> こんにちは! </h1>
+        <p style={contentStyle}> お元気ですか？ </p>
+        <button onClick={onClickButton}> ボタン </button>
         </>
         );
     };
