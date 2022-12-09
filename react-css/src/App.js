2cd612e446e1e96883e273fc380a51140e8b4d4c
clc95285 <yuya.izumi18@gmail.com>
2022-12-09 14:14:58 +0900

    5-4 styled components

--- react-css/src/App.js
+++ react-css/src/App.js
@@ -1,11 +1,13 @@
 import { CssModules } from "./components/CssModules";
 import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledConponents";
 
 export const App = () => {
     return(
         <div>
             <CssModules />
             <StyledJsx />
+            <StyledComponents />
         </div>
     );
 };
